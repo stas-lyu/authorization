94b599f0290abecfd2d450e5d3ce79c343dc8b96
stas-lyu <staslyu1997@gmail.com>
2021-06-17 10:07:34 +0300

    first commit

---
+++ authorization.js
@@ -0,0 +1,11 @@
+export function authWithEmailAndPassword(email, password) {
+    const apiKey = 'iWSRBretlAZbV1n6HpHpA7ZOiYG2'
+    return fetch(`rdwd`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email, password,
+                returnSecureToken: true,
+            })
+        }
+    )
+}
